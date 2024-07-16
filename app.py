d0a1eebc2e02b33c9d8bdb7cf7b4d3e2902a7471
KWY <kaiwei12014@gmail.com>
2024-07-16 14:10:28 +0800

    Get a specific store and its item (60)

--- app.py
+++ app.py
@@ -34,3 +34,18 @@ def create_item(name):
             store["items"].append(new_item)
             return new_item, 201
     return {"message": "Store not found"}, 404
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"massage": "Store not found"}, 404
+
+@app.get("/store/<string:name>/item")
+def get_item(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
+
