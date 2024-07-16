62eb9d36ba3ddce93a78c5a52bb575ffdab8f628
KWY <kaiwei12014@gmail.com>
2024-07-16 16:58:00 +0800

    Add PUT and DELETE endpoints for REST API

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ app = Flask(__name__)
 def get_stores():
     return {"stores": list(stores.values())}
 
+@app.get("/store/<string:store_id>")
+def get_store(store_id):
+    try:
+        return store[store_id]
+    except KeyError:
+        abort(404, message="Store not found")
+
 @app.post("/store")
 def create_store():
     store_data = request.get_json()
@@ -27,9 +34,37 @@ def create_store():
     stores[store_id] = store
     return store, 201
 
+@app.delete("/store/<string:store_id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message": "Store deleted."}
+    except KeyError:
+        abort(404, message="Store not found.")
+
+@app.put("/store/<string:store_id>")
+def update_store(store_id):
+    store_data = request.get_json()
+    if "name" not in store_data:
+        abort(400, message="Bad request. Ensure 'name' is included in the JSON payload.")
+    
+    try:
+        store = stores[store_id]
+        store |= store_data
+        return store
+    except KeyError:
+        abort(400, message="Item not found")
+
 @app.get("/item") # http://127.0.0.1:5000/store
 def get_all_items():
     return {"items": list(items.values())}
+
+@app.get("/item/<string:item_id>")
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort(404, message="Item not found")
 
 @app.post("/item")
 def create_item():
@@ -60,17 +95,24 @@ def create_item():
     items[item_id] = {**item_data, "id": item_id}
     return items[item_id], 201
 
-@app.get("/store/<string:store_id>")
-def get_store(store_id):
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
     try:
-        return store[store_id]
+        del items[item_id]
+        return {"message": "Item deleted."}
     except KeyError:
-        abort(404, message="Store not found")
+        abort(404, message="Item not found.")
 
-@app.get("/item/<string:item_id>")
-def get_item(item_id):
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data = request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400, message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
+    
     try:
-        return items[item_id]
+        item = items[item_id]
+        item |= item_data
+        return item
     except KeyError:
-        abort(404, message="Item not found")
+        abort(400, message="Item not found")
 
