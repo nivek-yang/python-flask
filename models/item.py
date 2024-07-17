7d576b420ba4e22810174dcb9ef2b333ea9c770c
KWY <kaiwei12014@gmail.com>
2024-07-17 14:23:40 +0800

    Build one-to-many relationship using SQLAlchemy (75)

--- models/item.py
+++ models/item.py
@@ -6,7 +6,8 @@ class ItemModel(db.Model):
     id = db.Column(db.Interger, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
     price = db.Column(db.Float(precision=2), unique=False, nullable=False)
-    store_id = db.Column(db.Interger, unique=False, nullable=False)
+    store_id = db.Column(db.Interger, db.Foreign_key("stores.id"), unique=False, nullable=False)
+    store = db.relationship("StoreModel", back_populates="items")
 
-    
+
 
--- schemas.py
+++ schemas.py
@@ -1,19 +1,28 @@
 from marshmallow import Schema, fields
 
-class ItemSchema(Schema):
+class PlainItemSchema(Schema):
     id = fields.Str(dump_only=True) # only for response
     name = fields.Str(required=True)
     price = fields.Float(required=True)
-    store_id = fields.Str(required=True)
+    
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
 
-class StoreSchema(Schema):
+class PlainStoreSchema(Schema):
     id = fields.Str(dump_only=True)
     name = fields.Str(required=True)
 
 class StoreUpdateSchema(Schema):
     name = fields.Str(required=True)
-    
+
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True, load_only=True)
+    # store = fields.Nested(lambda: StoreSchema(), dump_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True)  
+     
+class StoreSchema(PlainStoreSchema):
+    # items = fields.List(fields.Nested(lambda: ItemSchema()), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+
