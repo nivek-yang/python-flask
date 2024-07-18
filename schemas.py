6c4cd74faa0275a199a22eef38c9640f7a75aa5c
KWY <kaiwei12014@gmail.com>
2024-07-18 14:18:03 +0800

    Build user model and schema (91)

--- schemas.py
+++ schemas.py
@@ -41,3 +41,8 @@ class TagAndItemSchema(Schema):
     message = fields.Str()
     item = fields.Nested(ItemSchema)
     tag = fields.Nested(TagSchema)
+
+class UserSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+    password = fields.Str(required=True, load_only=True)
