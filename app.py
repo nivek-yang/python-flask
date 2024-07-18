f4e017196f102cc0f0a9bc778090a6c8488f67c9
KWY <kaiwei12014@gmail.com>
2024-07-18 16:05:12 +0800

    Add JWT claims to confirm whether a user is an admin (95)

--- app.py
+++ app.py
@@ -32,6 +32,13 @@ def create_app(db_url=None):
 
     jwt = JWTManager(app)
 
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        # Look in the database and see whether the user is an admin
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
     @jwt.expired_token_loader
     def expire_token_callback(jwt_header, jwt_payload):
         return (
