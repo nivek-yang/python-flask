decbf17631524cd8a082dfefde0051330ef63de6
KWY <kaiwei12014@gmail.com>
2024-07-18 11:31:57 +0800

    Create tag model and build one-to-many relationship between stores and tags (85)

--- schemas.py
+++ schemas.py
@@ -1,19 +1,22 @@
 from marshmallow import Schema, fields
 
 class PlainItemSchema(Schema):
-    id = fields.Str(dump_only=True) # only for response
+    id = fields.Int(dump_only=True) # only for response
     name = fields.Str(required=True)
     price = fields.Float(required=True)
     
+class PlainStoreSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
+
+class PlainTagSchema(Schema):
+    id = fields.Int(dump_only=True)
+    name = fields.Str(required=True)
 
 class ItemUpdateSchema(Schema):
     name = fields.Str()
     price = fields.Float()
     store_id = fields.Int()
-
-class PlainStoreSchema(Schema):
-    id = fields.Str(dump_only=True)
-    name = fields.Str(required=True)
 
 class StoreUpdateSchema(Schema):
     name = fields.Str(required=True)
@@ -26,4 +29,8 @@ class ItemSchema(PlainItemSchema):
 class StoreSchema(PlainStoreSchema):
     # items = fields.List(fields.Nested(lambda: ItemSchema()), dump_only=True)
     items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
+class TagSchema(PlainTagSchema):
+    store_id = fields.Int(load_only=True)
+    store = fields.Nested(PlainStoreSchema(), dump_only=True) 
