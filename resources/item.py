6934e0ee59da9629e63c7295afcd88b488ae02c6
KWY <kaiwei12014@gmail.com>
2024-07-18 15:04:57 +0800

    Add register endpoint to REST API and replace <string:[id]> to <int:[id]> (92)

--- resources/item.py
+++ resources/item.py
@@ -30,7 +30,7 @@ class ItemList(MethodView):
         return item
 
 
-@blp.route("/item/<string:item_id>")
+@blp.route("/item/<int:item_id>")
 class Item(MethodView):
     @blp.response(200, ItemSchema)
     def get(self,item_id):
--- resources/store.py
+++ resources/store.py
@@ -31,7 +31,7 @@ class StoreList(MethodView):
 
         return store, 201
     
-@blp.route("/store/<string:store_id>")
+@blp.route("/store/<int:store_id>")
 class Store(MethodView):
     @blp.response(200, StoreSchema)
     def get(self, store_id):
