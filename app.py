a894aa7d1993bdbcb2993612013cec4bb32d9a10
KWY <kaiwei12014@gmail.com>
2024-07-19 15:45:33 +0800

    Add Flask-Migrate to Flask app

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from flask import Flask, jsonify
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
+from flask_migrate import Migrate
 
 from db import db
 from blocklist import BLOCKLIST
@@ -26,7 +27,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
-
+    migrate = Migrate(app, db)
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = "my_secret" # need to move to .env
@@ -87,8 +88,8 @@ def create_app(db_url=None):
         ), 401,
         )
 
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():  # Remove while using Flask-Migrate to create Database
+    #     db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
